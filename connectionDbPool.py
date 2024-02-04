115b5819f8b258ea181c5c51d594e97522097127
Kwiatek90 <mateusz.kwiat9@gmail.com>
2024-02-04 23:01:28 +0100

    first commit

---
+++ connectionDbPool.py
@@ -0,0 +1,85 @@
+import psycopg2
+import config
+import threading
+from queue import Queue, Empty
+import time  
+
+
+
+class ConnectionPool():
+    def __init__(self, database_config_path):
+        self.database_config_path = database_config_path
+        self.min_queue_conn = 10
+        self.max_conn = 50
+        self.queue = Queue(maxsize=self.max_conn)
+        self.active_conn = 0  
+        self.rel_conn = 0
+        self.start_time = time.time()
+        self.semaphore = threading.Semaphore()
+        self.open_threads_connections()
+        self.cleanup_thread = threading.Thread(target=self.cleanup_connections, daemon=True)
+        self.cleanup_thread.start()
+        self.check_conn = threading.Thread(target=self.check_conn, daemon=True)
+        self.check_conn.start()
+        
+    def open_threads_connections(self):
+        '''At the beginning create a 10 locked connections in queue'''
+        
+        with self.semaphore:
+            for _ in range(self.min_queue_conn):
+                self.queue.put(self.connect_db())
+    
+    def connect_db(self):
+        """Connect to the PostgreSQL database server"""
+
+        try:
+            params = config.config_params(self.database_config_path)
+            conn = psycopg2.connect(**params)
+            return conn
+        except Exception as e:
+            return e
+            
+        
+    def get_conn(self):
+        '''Take the connection from queue, if queue is empty, create one'''
+        
+        with self.semaphore:
+          
+            try:
+                conn_db = self.queue.get_nowait()
+                self.active_conn += 1
+            except Empty:
+                if (self.queue.qsize() + self.active_conn) < self.max_conn:
+                    conn_db = self.connect_db()
+                    self.active_conn +=1
+                else:  
+                    raise ValueError("Too much connections")
+            
+            return conn_db
+                   
+    def release_conn(self, conn_db):
+        '''Release connection to the queue'''
+        
+        with self.semaphore:
+            self.queue.put(conn_db) 
+            self.rel_conn += 1
+            self.active_conn -= 1
+            
+                    
+    def cleanup_connections(self):
+        '''Every 60 seconds function check connections'''
+        
+        while True:
+            time.sleep(60)
+            with self.semaphore:
+                while not self.queue.empty():
+                    if self.queue.qsize() > self.min_queue_conn:
+                        conn_db_obj = self.queue.get()
+                        conn_db_obj.close()
+                        
+                        
+    def check_conn(self):
+        while True:
+            time.sleep(2)
+            print(f"Active conn: {self.active_conn}\nRelease conn: {self.rel_conn}\nQueue size: {self.queue.qsize()}\n")
+            
---
+++ test_database.py
@@ -0,0 +1,45 @@
+import unittest
+import time
+import connectionDbPool
+import threading
+
+class DatabaseTests(unittest.TestCase):
+    def setUp(self):
+        self.connPool = connectionDbPool.ConnectionPool("database.ini")
+        
+    def test_crash_with_5000_query_to_databsae_using_threads(self):
+        
+        def worker(i):
+            
+            try:
+                query = "SELECT * FROM users;"
+                conn_db = self.connPool.get_conn()
+                
+                with conn_db.cursor() as cur:
+                    cur.execute(query)
+                    response =  cur.fetchall()
+                    self.connPool.release_conn(conn_db)
+                    #print(f'Thread number: {i}')
+            except Exception as e:
+                pass
+                #print(f"Thread not executed: {i}, error: {e}")
+                
+        start = time.perf_counter()
+        
+        threads = []
+        
+        for i in range(200000):
+            thread = threading.Thread(target=worker, args=(i,))
+            thread.start()
+            threads.append(thread)
+            
+        for thread in threads:
+            thread.join()
+
+        finish = time.perf_counter()
+        print(f"Finished in {round(finish-start,2)} second(s)")
+
+
+if __name__ == "__main__": 
+    unittest.main()
+        
